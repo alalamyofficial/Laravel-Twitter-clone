298068a8f97b9a006625263c21882190730abe00
alalamyofficial <theories619@gmail.com>
2021-08-26 22:11:08 +0200

    twitter9.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
                  <img src="sm.gif" alt="">   
 
                 <button class="py-2 px-4 text-lg bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Tweety
+                    Tooty
                 </button>
 
             </div>
